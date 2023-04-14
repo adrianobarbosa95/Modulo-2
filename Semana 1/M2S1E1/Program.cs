ea36de32f27ab8a63830d50c5899ff41a5c28f7d
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-04-14 20:33:01 -0300

    first commit

---
+++ Semana 1/M2S1E1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace M2S1E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro numero inteiro");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero inteiro");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(numero1+numero2);
+        }
+    }
+}
