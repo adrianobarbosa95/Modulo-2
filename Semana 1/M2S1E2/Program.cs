e6902b8e081fa822f4e75d52e7c0342e30af67cc
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-04-14 20:47:18 -0300

    Questão 2

---
+++ Semana 1/M2S1E2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace M2S1E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine((numero % 2 == 0) ? "É par" : "é impar");
+        }
+    }
+}
