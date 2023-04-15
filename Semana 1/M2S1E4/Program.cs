4e4c1bea35a3b0332822978130a0afb3e7737b93
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-04-14 22:05:32 -0300

    Exercicio 4

---
+++ Semana 1/M2S1E4/Program.cs
@@ -0,0 +1,21 @@
+﻿ 
+using System;
+// Faça um programa que leia a Marca, Modelo e  km rodados e mostre as informações do 
+// carro e se a KM for acima 10 mil, apresentar a mensagem que precisa ser feito uma revisão.
+namespace M2S1E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Digite a marca do carro:");
+        string marca = Console.ReadLine();
+        Console.WriteLine("Digite o Modelo do carro:");
+        string modelo = Console.ReadLine();
+        Console.WriteLine("Digite o km rodados do carro:");
+        float km = float.Parse(Console.ReadLine());
+        Console.WriteLine($"O carro da {marca}, modelo {modelo} tem {km}km rodados!");
+        if(km > 10000) Console.WriteLine("Precisa ser feito uma revisão");
+        }
+    }
+}
---
+++ Semana 1/M2S1E5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M2S1E5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
