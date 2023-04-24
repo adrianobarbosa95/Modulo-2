2636fddb95e97cda1d442942060f7f1720dceeea
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-04-24 20:27:22 -0300

    criar exercicio 01 semana 2

---
+++ Semana2/M2S03E1.cs
@@ -0,0 +1,11 @@
+namespace Semana2
+{
+    public class M2S03E1
+    {
+        /* Crie um método que receba por parâmetro dois números inteiros e retorne a soma deles.*/
+        public int soma(int  numero, int numero2){
+            return numero + numero2;
+        }
+
+    }
+}
---
+++ Semana2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Semana2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          int soma = new M2S03E1().soma(3, 5);
+          Console.WriteLine(soma);
+          
+
+        }
+    }
+}
